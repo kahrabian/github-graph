c79b69db97460b9f46b4c68695672da4b5419bb9
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2020-01-27 02:20:01 -0500

    fix visualize local job

--- resources/compute_canada/cc_visualize_local.sh
+++ resources/compute_canada/cc_visualize_local.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --account=def-jinguo
 #SBATCH --job-name=gg-visualize_local
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=128G
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kian.ahrabian@mail.mcgill.ca
 
 source ~/venvs/gg/bin/activate
 
-export TRD_CNT=16
+export TRD_CNT=32
 export ST_TM=2019-12-01-00
 export SP_TM=2020-01-01-00
 export VIZ_CNT=10
-export VIZ_D=5
-export MD=S
+export VIZ_D=10
+export MD=G
 python src/visualize_local.py
