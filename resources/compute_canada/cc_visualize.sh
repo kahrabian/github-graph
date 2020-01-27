2708e86ef32c2ac498dd3a4438ee09fd9c4e0945
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2020-01-27 01:05:55 -0500

    add visualizing codes

--- resources/compute_canada/cc_visualize.sh
+++ resources/compute_canada/cc_visualize.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-jinguo
 #SBATCH --job-name=gg-sample
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=16G
+#SBATCH --mem=128G
 #SBATCH --time=0:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kian.ahrabian@mail.mcgill.ca
@@ -12,7 +12,6 @@ source ~/venvs/gg/bin/activate
 export TRD_CNT=16
 export ST_TM=2019-12-01-00
 export SP_TM=2020-01-01-00
-export IN_SZ=2000
-export TG_SZ=2000000
-export SMPL_RT=2000
-python src/sample.py
+export VIZ_CNT=10
+export VIZ_D=5
+python src/visualize_local.py
---
+++ src/visualize_local.py
@@ -0,0 +1,187 @@
+import glob
+import itertools
+import matplotlib.pyplot as plt
+import networkx as nx
+import os
+import queue
+import random
+import re
+
+from datetime import datetime
+from queue import PriorityQueue
+from threading import Lock, Thread
+
+
+def _extract(g, lk, trd_cnt, prt):
+    st_tm = datetime.strptime(os.getenv('ST_TM', ''), '%Y-%m-%d-%H')
+    sp_tm = datetime.strptime(os.getenv('SP_TM', ''), '%Y-%m-%d-%H')
+    for fn in glob.glob('./data/graph/*.txt'):
+        fn_tm = datetime.strptime(fn, './data/graph/%Y-%m-%d-%H.txt')
+        if fn_tm < st_tm or fn_tm > sp_tm or fn_tm.hour % trd_cnt != prt:
+            continue
+        with open(fn, 'r') as fr:
+            for l in fr.readlines():
+                v1, r, v2, t = l.split('\t')
+                with lk:
+                    if v1 not in g:
+                        g[v1] = set()
+                    g[v1].add(v2)
+
+                    # NOTE: To find nodes with the most interactions
+                    if v2 not in g:
+                        g[v2] = set()
+                    g[v2].add(v1)
+
+
+def extract():
+    g = {}
+    lk = Lock()
+    trd_cnt = int(os.getenv('TRD_CNT', '16'))
+    for i in range(0, trd_cnt):
+        t = Thread(target=_extract, args=(g, lk, trd_cnt, i))
+        t.start()
+        t.join()
+    return g
+
+
+def _transform(tg, g, gk, lk, trd_cnt, prt):
+    for i, v in enumerate(gk):
+        if i % trd_cnt != prt:
+            continue
+        tp_rx = re.compile(r'/(.*)/.*')
+        tp = re.findall(tp_rx, v)[0]
+        nv = len(g[v])
+        with lk:
+            if tp not in tg:
+                tg[tp] = set()
+            tg[tp].add((nv, v))
+
+
+def transform(g):
+    tg = {}
+    lk = Lock()
+    gk = sorted(g.keys())
+    trd_cnt = int(os.getenv('TRD_CNT', '16'))
+    for i in range(0, trd_cnt):
+        t = Thread(target=_transform, args=(tg, g, gk, lk, trd_cnt, i))
+        t.start()
+        t.join()
+    return tg
+
+
+def _bfs(viz_g, g, viz_pl, viz_d, trd_cnt, prt):
+    for i, (dx, x) in enumerate(viz_pl):
+        if i % trd_cnt != prt:
+            continue
+        tp_rx = re.compile(r'/(.*)/(.*)')
+        tp, pk = re.findall(tp_rx, x)[0]
+
+        vs = set()
+        mk = set()
+        q = queue.Queue()
+        mk.add(x)
+        q.put((x, 0))
+        while not q.empty():
+            v, d = q.get()
+            if d > viz_d:
+                continue
+            vs.add(v)
+            for n in g[v]:
+                if n not in mk:
+                    mk.add(n)
+                    q.put((n, d + 1))
+
+        k = f'{dx}_{tp}_{pk}'
+        for v in vs:
+            if k not in viz_g:
+                viz_g[k] = set()
+            for u in (vs & g[v]):
+                # NOTE: To reduce memory footprint
+                viz_g[k].add((max(v, u), min(v, u)))
+
+
+def bfs(g, tg):
+    viz_cnt = int(os.getenv('VIZ_CNT', '10'))
+    viz_pl = []
+    for k, v in tg.items():
+        viz_pl += sorted(v, reverse=True)[:viz_cnt]
+
+    viz_g = {}
+    lk = Lock()
+    viz_d = int(os.getenv('VIZ_D', '3'))
+    trd_cnt = int(os.getenv('TRD_CNT', '16'))
+    for i in range(0, trd_cnt):
+        t = Thread(target=_bfs, args=(viz_g, g, viz_pl, viz_d, trd_cnt, i))
+        t.start()
+        t.join()
+    return viz_g
+
+
+def _build(plt_g, viz_g, viz_gk, gp, cm, lk, trd_cnt, prt):
+    for i, k in enumerate(viz_gk):
+        if i % trd_cnt != prt:
+            continue
+        vs = []
+        tp_rx = re.compile(r'/(.*)/.*')
+        for v in set(itertools.chain.from_iterable(viz_g[k])):
+            tp = re.findall(tp_rx, v)[0]
+            vs.append((v, {'group': gp[tp], 'color': cm[tp]}))
+
+        net = nx.Graph()
+        net.add_nodes_from(vs)
+        net.add_edges_from(viz_g[k])
+
+        plt_g.append((net, k))
+
+
+def build(viz_g):
+    from configs.graph import schema as cfg_schema
+
+    cm = {}
+    gp = {}
+    cp = ['#111d5e', '#46b5d1', '#b21f66', '#f65c78', '#2d334a', '#c3f584', '#ff7315', '#ffd271', '#fff3af']
+    for i, tp in enumerate(sorted(cfg_schema.schema.keys())):
+        gp[tp] = i
+        cm[tp] = cp[i]
+
+    plt_g = []
+    lk = Lock()
+    viz_gk = sorted(viz_g.keys())
+    trd_cnt = int(os.getenv('TRD_CNT', '16'))
+    for i in range(0, trd_cnt):
+        t = Thread(target=_build, args=(plt_g, viz_g, viz_gk, gp, cm, lk, trd_cnt, i))
+        t.start()
+        t.join()
+    return plt_g
+
+
+def plot(plt_g):
+    for net, k in plt_g:
+        tp_rx = re.compile(r'([^_]*)_(.*)_([^_]*)')
+        d, tp, pk = re.findall(tp_rx, k)[0]
+
+        dir_pth = f'./results/visualizations/local/{tp}'
+        if not os.path.isdir(dir_pth):
+            os.mkdir(dir_pth)
+
+        fig = plt.figure(figsize=(100, 100))
+        ax = fig.add_subplot(111)
+
+        pos = nx.fruchterman_reingold_layout(net)
+        nx.draw_networkx(net, pos)
+
+        plt.tight_layout()
+        plt.savefig(f'{dir_pth}/{pk}_{d}.png', format='PNG')
+
+
+def main():
+    g = extract()
+    tg = transform(g)
+    viz_g = bfs(g, tg)
+    plt_g = build(viz_g)
+    plot(plt_g)
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
