42f213d69ed1a9a0c05df0ccb00e8386ee684d84
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2020-01-27 17:22:38 -0500

    update requirements.txt

--- resources/compute_canada/visualize_local.template.sh
+++ resources/compute_canada/visualize_local.template.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --account=def-jinguo
 #SBATCH --job-name=gg-visualize_local
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64G
-#SBATCH --time=3:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kian.ahrabian@mail.mcgill.ca
 
 source ~/venvs/gg/bin/activate
 
-export TRD_CNT=16
+export TRD_CNT=8
 export ST_TM=2019-12-01-00
 export SP_TM=2020-01-01-00
-export VIZ_CNT=5
-export VIZ_D=5
+export VIZ_CNT=3
+export VIZ_D=3
 export MD=S
 python src/visualize_local.py
