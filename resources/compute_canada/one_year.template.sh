3d9b321cb83c29cf9dda12dc1c54a8085ba8d35e
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2021-01-15 03:47:36 -0500

    update compute canada resources

--- resources/compute_canada/one_year.template.sh
+++ resources/compute_canada/one_year.template.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=def-jinguo
 #SBATCH --job-name=gg-one-year
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=350G
-#SBATCH --time=1-0
+#SBATCH --time=12:00:00
 #SBATCH --output=./logs/%x-%j.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kian.ahrabian@mail.mcgill.ca
 
 module load python/3
 
-export TRD_CNT=20
+export TRD_CNT=32
 python src/one_year.py
